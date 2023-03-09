7ead20969de80a251be5edda6a1ecff72a1cf04b
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2023-03-09 15:07:03 -0800

    merged shayan into main

---
+++ MediaPlayer/MediaPlayer/VideoLibraryMdiChild.Designer.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    partial class VideoLibraryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(21, 280);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1418, 629);
+            this.listBox1.TabIndex = 0;
+            // 
+            // MusicLibraryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1466, 937);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "MusicLibraryForm";
+            this.Text = "MusicLibraryForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+    }
+}
---
+++ MediaPlayer/MediaPlayer/VideoLibraryMdiChild.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class VideoLibraryForm : Form
+    {
+        public VideoLibraryForm()
+        {
+            InitializeComponent();
+            fillList();
+            listBox1.Width = (Width);
+        }
+
+        void format(ref StringBuilder stringBuilder)
+        {
+            if (stringBuilder.Length >= 50)
+            {
+                int lengthTaken = stringBuilder.Length - 50;
+                for (int j = lengthTaken; j > 0; j--)
+                {
+                    stringBuilder.Remove(stringBuilder.Length - 1, stringBuilder.Length - 1);
+                }
+            }
+            if (stringBuilder.Length < 50)
+            {
+                int lengthAdded = 50 - stringBuilder.Length;
+                for (int j = 0; j < lengthAdded - 1; j++)
+                {
+                    stringBuilder.Append(' ');
+                }
+            }
+
+        }
+
+        void fillList()
+        {
+            for (int i = 0; i < MediaScanner.Videos.Count; i++)
+            {
+                StringBuilder titleBuilder = new StringBuilder();
+                StringBuilder artistBuilder = new StringBuilder();
+                StringBuilder durationBuilder = new StringBuilder();
+                if (MediaScanner.Videos[i] != null)
+                {
+                    titleBuilder.Append(MediaScanner.Videos[i].title);
+                    titleBuilder.Append(MediaScanner.Videos[i].director);
+                    if (MediaScanner.Videos[i].genres == null)
+                    {
+                        artistBuilder.Append("Unknown");
+                    }
+                    else
+                    {
+                        artistBuilder.Append(MediaScanner.Audios[i].getArtists());
+                    }
+                    if (MediaScanner.Audios[i].duration == null)
+                    {
+                        durationBuilder.Append("Unknown");
+                    }
+                    else
+                    {
+                        durationBuilder.Append(MediaScanner.Audios[i].duration.ToString());
+                    }
+
+                    format(ref titleBuilder);
+                    format(ref artistBuilder);
+                    listBox1.Items.Add(titleBuilder.ToString() + artistBuilder.ToString() + durationBuilder.ToString());
+                }
+            }
+        }
+    }
+}

0a1818062659cb059b4ac96bb1668edbfd02f38e
dmc <dchellapan@gmail.com>
2023-03-26 15:17:48 -0700

    added test cases for audio in UnitTestAudio.cs

---
+++ MediaPlayer/UnitTestAudio/UnitTestAudio.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MediaPlayer;
+using System.Text;
+
+namespace UnitTestMediaPlayerAudio
+{
+    /// <summary>
+    /// Test class to test the functionality in Audio.cs
+    /// By Daniel Chellapan
+    /// </summary>
+    [TestClass]
+    public class UnitTestAudio
+    {
+        /// <summary>
+        /// Tests property   for audio title
+        /// By Daniel Chellapan
+        /// </summary>
+        [TestMethod]
+        public void TestAudioTitleProperty()
+        {
+            Audio title = new Audio();
+            title.title = "Numb";
+            Assert.AreEqual(title.title, "Numb");
+        }
+
+        /// <summary>
+        /// Tests getArtist function logic 
+        /// By Daniel Chellapan
+        /// </summary>
+        [TestMethod]
+        public void TestGetArtists()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] artistNamesArray = { "Jay-Z", "Jim", "Joe" };
+            for (int i = 0; i < artistNamesArray.Length; i++)
+            {
+                if (i == artistNamesArray.Length - 1)
+                {
+                    stringBuilder.Append(artistNamesArray[i]);
+                }
+                else
+                {
+                    stringBuilder.Append(artistNamesArray[i] + ", ");
+                }
+                stringBuilder.ToString();
+            }
+
+
+
+            Audio artists = new Audio();
+            artists.getArtists();
+
+            Assert.AreEqual(artistNamesArray[0], "Jay-Z");
+        }
+
+
+    }
+}

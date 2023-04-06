178285fe004f404de0b6315027e7993c3c49c67b
Daniel Michael Chellapan <113056515+danielmchellapan@users.noreply.github.com>
2023-04-06 02:28:37 -0700

    Update UnitTestPlaylist.cs
    visual studio git version control issue

--- MediaPlayer/UnitTestPlayList/UnitTestPlaylist.cs
+++ MediaPlayer/UnitTestPlayList/UnitTestPlaylist.cs
@@ -6,9 +6,42 @@ namespace UnitTestPlayList
     [TestClass]
     public class UnitTest1
     {
+         /// <summary>
+        /// Tests to see if the playlist Constructor is not null
+        /// By Daniel Chellapan
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestConstructorNotNull()
         {
+            string name = "My Playlist";
+            Playlist playlist = new Playlist(name);
+            Assert.IsNotNull(playlist.Songs);
+
+        }
+        
+        /// <summary>
+        /// Tests to see if the playlist 
+        /// By Daniel Chellapan
+        /// </summary>
+        [TestMethod]
+        public void TestConstructorCodeLengthMakesSense()
+        {
+            string name = "My Playlist";
+            Playlist playlist = new Playlist(name);
+            Assert.AreEqual(0, playlist.Songs.Count);
+        }
+        
+         /// <summary>
+        /// Tests getArtist function logic 
+        /// By Daniel Chellapan
+        /// </summary>
+        [TestMethod]
+        public void TestConstructorNameMakesSense()
+        {
+            string name = "My Playlist";
+            Playlist playlist = new Playlist(name);
+
+            Assert.AreEqual(name, playlist.Name);
         }
     }
 }

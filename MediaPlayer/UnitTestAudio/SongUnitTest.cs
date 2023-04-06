c7b49e5b95c07a72d10de7b40a7a4f55e4775f90
dmc <dchellapan@gmail.com>
2023-04-06 02:22:19 -0700

    Added more unit tests

--- MediaPlayer/UnitTestAudio/SongUnitTest.cs
+++ MediaPlayer/UnitTestAudio/SongUnitTest.cs
@@ -8,67 +8,132 @@ using System.Threading.Tasks;
 
 namespace UnitTestSong
 {
-    /// <summary>
-    /// Tests to see if the playlist Constructor is not null
-    /// By Daniel Chellapan
-    /// </summary>
-    [TestClass]
-    public class SongUnitTest
-    {
+        /// <summary>
+        /// Tests to see if the playlist Constructor is not null
+        /// By Daniel Chellapan
+        /// </summary>
         [TestClass]
-        public class PlaySongUnitTest
+        public class SongUnitTest
         {
-            /// <summary>
-            /// Test class to test the functionality in Audio.cs
-            /// By Daniel Chellapan
-            /// </summary>
             [TestClass]
-            public class UnitTestAudio
+            public class PlaySongUnitTest
             {
                 /// <summary>
-                /// Tests property   for audio title
+                /// Test class to test the functionality in Audio.cs
                 /// By Daniel Chellapan
                 /// </summary>
-                [TestMethod]
-                public void TestSongTitleProperty()
+                [TestClass]
+                public class UnitTestSong
                 {
-                    Song title = new Song();
-                    title.Title = "Numb";
-                    Assert.AreEqual(title.Title, "Numb");
-                }
-
-                /// <summary>
-                /// Tests getArtist function logic 
-                /// By Daniel Chellapan
-                /// </summary>
-                [TestMethod]
-                public void TestGetArtists()
-                {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    string[] artistNamesArray = { "Jay-Z", "Jim", "Joe" };
-                    for (int i = 0; i < artistNamesArray.Length; i++)
+                    /// <summary>
+                    /// Tests Title property 
+                    /// By Daniel Chellapan
+                    /// </summary>
+                    [TestMethod]
+                    public void TitlePropertyShouldSetAndGetCorrectValue()
                     {
-                        if (i == artistNamesArray.Length - 1)
-                        {
-                            stringBuilder.Append(artistNamesArray[i]);
-                        }
-                        else
-                        {
-                            stringBuilder.Append(artistNamesArray[i] + ", ");
-                        }
-                        stringBuilder.ToString();
+
+                        Song song = new Song();
+                        string expectedTitle = "Example Song";
+                        song.Title = expectedTitle;
+                        string actualTitle = song.Title;
+                        Assert.AreEqual(expectedTitle, actualTitle);
                     }
 
 
+                    /// <summary>
+                    /// Tests property FileLocation
+                    /// By Daniel Chellapan
+                    /// </summary>
+                    [TestMethod]
+                    public void FileLocationPropertyShouldSetAndGetCorrectValue()
+                    {
 
-                    Song artists = new Song();
-                    artists.GetArtists();
+                        Song song = new Song();
+                        string expectedFileLocation = @"C:\MediaPlayer\ThisSong.mp3";
+                        song.FileLocation = expectedFileLocation;
+                        string actualFileLocation = song.FileLocation;
+                        Assert.AreEqual(expectedFileLocation, actualFileLocation);
+                    }
 
-                    Assert.AreEqual(artistNamesArray[0], "Jay-Z");
+                    /// <summary>
+                    /// Tests property AlbumProperty
+                    /// By Daniel Chellapan
+                    /// </summary>
+                    [TestMethod]
+                    public void AlbumPropertyShouldSetAndGetCorrectValue()
+                    {
+
+                        Song song = new Song();
+                        string expectedAlbum = "Example Album";
+                        song.Album = expectedAlbum;
+                        string actualAlbum = song.Album;
+                        Assert.AreEqual(expectedAlbum, actualAlbum);
+                    }
+
+                    /// <summary>
+                    /// Tests property GenreProperty
+                    /// By Daniel Chellapan
+                    /// </summary>
+                    [TestMethod]
+                    public void GenresPropertyShouldSetAndGetCorrectValue()
+                    {
+
+                        Song song = new Song();
+                        string[] expectedGenres = new string[] { "Genre 1", "Genre 2" };
+                        song.Genres = expectedGenres;
+                        string[] actualGenres = song.Genres;
+                        CollectionAssert.AreEqual(expectedGenres, actualGenres);
+                    }
+
+                    /// <summary>
+                    /// Tests property DurationProperty
+                    /// By Daniel Chellapan
+                    /// </summary>
+                    [TestMethod]
+                    public void DurationPropertyShouldSetAndGetCorrectValue()
+                    {
+
+                        Song song = new Song();
+                        string expectedDuration = "3:45";
+                        song.Duration = expectedDuration;
+                        string actualDuration = song.Duration;
+                        Assert.AreEqual(expectedDuration, actualDuration);
+                    }
+
+                    /// <summary>
+                    /// Tests GetArtist function logic 
+                    /// By Daniel Chellapan
+                    /// </summary>
+                    [TestMethod]
+                    public void TestGetArtists()
+                    {
+                        StringBuilder StringBuilder = new StringBuilder();
+                        string[] artistNamesArray = { "Jay-Z", "Jim", "Joe" };
+                        for (int i = 0; i < artistNamesArray.Length; i++)
+                        {
+                            if (i == artistNamesArray.Length - 1)
+                            {
+                                StringBuilder.Append(artistNamesArray[i]);
+                            }
+                            else
+                            {
+                                StringBuilder.Append(artistNamesArray[i] + ", ");
+                            }
+                            StringBuilder.ToString();
+                        }
+
+                        Song artists = new Song();
+                        artists.GetArtists();
+
+                        Assert.AreEqual(artistNamesArray[0], "Jay-Z");
+                    }
+
+
                 }
-
-
             }
         }
-    }
-}
+ }
+
+
+  
